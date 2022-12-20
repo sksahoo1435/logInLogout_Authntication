48c1286fc88e7f83497043c67162e09b63b22764
sunil kumar sahoo <sk7997717@gmail.com>
2022-12-21 02:34:12 +0530

    the login and the token save in context

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
-
+import { useContext } from 'react';
 import classes from './MainNavigation.module.css';
+import AuthContext from '../../store/auth-context';
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,15 +14,23 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to='/auth'>Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to='/profile'>Profile</Link>
+            </li>
+          )}
+
+          {isLoggedIn && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
+
         </ul>
       </nav>
     </header>
