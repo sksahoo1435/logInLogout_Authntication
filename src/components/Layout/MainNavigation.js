3f8266cdd4a541bbf8aca38474f7433284c3ce36
sunil kumar sahoo <sk7997717@gmail.com>
2022-12-21 02:42:27 +0530

    logout button work fine

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -7,6 +7,12 @@ const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
 
   const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = ()=>{
+    authCtx.logout();
+
+  };
+
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -27,7 +33,7 @@ const MainNavigation = () => {
 
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
 
